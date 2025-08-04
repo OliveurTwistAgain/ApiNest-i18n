1a8634aa6b9c3e420ae84a72ba07bd5311a885d4
OliveurTwistAgain <oliveur.twist.again@proton.me>
2025-08-04 19:12:50 +0200

    Fix icon

--- src/utils/localCoverList.js
+++ src/utils/localCoverList.js
@@ -1,5 +1,13 @@
 // AUTO-GENERATED. Run 'node generate-cover-list.js' to update.
 export const localCoverList = [
-
+    "cover001.jpg",
+    "cover002.jpg",
+    "cover003.jpg",
+    "cover004.jpg",
+    "cover005.jpg",
+    "cover006.jpg",
+    "cover007.jpg",
+    "cover008.jpg",
+    "cover009.jpg"
 ];
 
